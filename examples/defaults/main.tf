c96b532f61111a56a9bbd197ad1625953d983792
oycyc <yangciouou@gmail.com>
2025-01-30 11:32:45 -0500

    trunk

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -1,5 +1,5 @@
-# trunk-ignore(trivy/AVD-AWS-0143): False positive
-# trunk-ignore(trivy/AVD-AWS-0132): False positive
+# trunk-ignore-all(trivy/AVD-AWS-0143): False positive
+# trunk-ignore-all(trivy/AVD-AWS-0132): False positive
 provider "aws" {
   region = "us-east-1"
 }
